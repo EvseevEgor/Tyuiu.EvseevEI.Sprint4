e2dbd4171c4a9b27a997d36c193ee5ea69bb187f
Евсеев Егор <Tyuiu.EvseevEI@outlook.com>
2024-11-26 20:02:30 +0500

    1

---
+++ Tyuiu.EvseevEI.Sprint4.Task1.V13/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.EvseevEI.Sprint4.Task1.V13.Lib;
+namespace Tyuiu.EvseevEI.Sprint4.Task1.V13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService dataService = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнилa: Евсеев Е.И | ИИПБ-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Обработка структурных типов                                       *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #13                                                             *");
+            Console.WriteLine("* Выполнилa: Евсеев Егор Игоревич | ИИПб-24-2                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан одномерный целочисленный массив на 10 элементов заполненный         *");
+            Console.WriteLine("* значениями с клавиатуры в диапазоне от 0 до 8 подсчитать сумму четных   *");
+            Console.WriteLine("* элементов массива.  С клавиатуры: 8, 1, 5, 4, 2, 1, 2, 7, 6, 1          *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Введите длину массива: ");
+            int len = Convert.ToInt32(Console.ReadLine());
+
+            int[] numsArray = new int[len];
+
+            for (int i = 0; i < len; i++)
+            {
+                Console.Write("Введите значение {0} элемента массива: ", i);
+                numsArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+    }
+}
